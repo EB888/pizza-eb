ccf09110e385114a2125d1e78bcc75508ab138c4
Evan Bishop <evan.bishop888@gmail.com>
2017-03-03 15:39:14 -0800

    add multiple options for sizes and add hover effects to text

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,30 @@
-// BACK
-var finalToppingsArray = [];
-
-function Pizza(size){
+// BACK END
+function Pizza(size, toppings){
   this.size = size;
+  this.toppings = [];
 }
 
-Pizza.prototype.calculate = function(array) {
+Pizza.prototype.calculate = function(toppingArray) {
   var toppingDollars = 0;
-  for(i=0;i<array.length;i++){
-    toppingDollars += array[i]
+
+  for(i=0;i<toppingArray.length;i++){
+    toppingDollars += toppingArray[i]
   }
   var finalDollars = toppingDollars + this.size;
   return finalDollars;
 }
 
-// FRONT
+// FRONT END
 $(function(){
   $("#orderForm").submit(function(event){
     event.preventDefault();
     $("#totalPriceSpan").empty();
-    finalToppingsArray = [];
-    var finalSize = parseInt($("input:checkbox[name=size]:checked").val());
-    $("input:checkbox[name=toppings]:checked").each(function(){
-      finalToppingsArray.push(parseInt($(this).val()));
-    });
+    var finalSize = parseInt($("input:radio[name=size]:checked").val());
     var finalPizza = new Pizza(finalSize);
-    var outputCost = finalPizza.calculate(finalToppingsArray);
+    $("input:checkbox[name=toppings]:checked").each(function(){
+      finalPizza.toppings.push(parseInt($(this).val()));
+    });
+    var outputCost = finalPizza.calculate(finalPizza.toppings);
     $("#totalPriceSpan").append(outputCost);
     $("#totalPriceDiv").show();
   });
