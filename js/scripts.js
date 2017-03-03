c72a90330ede3e46d635ad715bbf0bc7d7106a6c
Evan Bishop <evan.bishop888@gmail.com>
2017-03-03 14:23:34 -0800

    fix bugs by emptying array of toppings when order is submitted, and cleaned up commented out code

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,17 @@
 // BACK
-var finalToppings = 0;
 var finalToppingsArray = [];
 
-function Pizza(sizeCost, toppingsCost){
-  this.sizeCost = sizeCost;
-  this.toppingsCost = toppingsCost;
+function Pizza(size){
+  this.size = size;
 }
 
-Pizza.prototype.calculate = function() {
-  // var totalCost = this.sizeCost + this.toppingsCost;
-  // return totalCost;
-  var count = 0;
-  for(i=0;i<finalToppingsArray.length;i++){
-    count += finalToppingsArray[i]
+Pizza.prototype.calculate = function(array) {
+  var toppingDollars = 0;
+  for(i=0;i<array.length;i++){
+    toppingDollars += array[i]
   }
-  // need to add the value of all indexes of the topping array together, then add to size cost
+  var finalDollars = toppingDollars + this.size;
+  return finalDollars;
 }
 
 // FRONT
@@ -22,14 +19,14 @@ $(function(){
   $("#orderForm").submit(function(event){
     event.preventDefault();
     $("#totalPriceSpan").empty();
+    finalToppingsArray = [];
     var finalSize = parseInt($("input:checkbox[name=size]:checked").val());
     $("input:checkbox[name=toppings]:checked").each(function(){
-      // finalToppings = finalToppings + parseInt($(this).val());
-      finalToppingsArray.push(parseInt($(this).val());
+      finalToppingsArray.push(parseInt($(this).val()));
     });
-    var finalPizza = new Pizza(finalSize, finalToppings);
-    var finalCost = finalPizza.calculate();
-    $("#totalPriceSpan").append(finalCost);
+    var finalPizza = new Pizza(finalSize);
+    var outputCost = finalPizza.calculate(finalToppingsArray);
+    $("#totalPriceSpan").append(outputCost);
     $("#totalPriceDiv").show();
   });
 });
